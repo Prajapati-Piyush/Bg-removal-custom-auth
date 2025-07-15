a6890d8fe172b4e6276a36358f75501b2b7191e6
Piyush <piyushprajapati0507@gmail.com>
2025-07-15 13:44:51 +0530

    Enable cross-site scriptiong

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -146,7 +146,7 @@ const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Strict',
+            sameSite: 'None',
         });
 
         return res.json({
