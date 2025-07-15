2613a7a85d032f5fcdd463be1894b42bd13484c6
Piyush <piyushprajapati0507@gmail.com>
2025-07-15 12:58:46 +0530

    FinalCommit

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "https://free-bg-removal.vercel.app" }));
+app.use(cors({ credentials: true, origin: "https://free-bg-removal.vercel.app", optionsSuccessStatus: 200 }));
 app.use(cookieParser());
 
 
